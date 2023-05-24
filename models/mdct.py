e65c712d2e06c749ab666b383fb5b3977a79d927
Chenhao <shua0003@e.ntu.edu.sg>
2023-05-24 12:17:38 +0800

    Format

--- models/mdct.py
+++ models/mdct.py
@@ -1,19 +1,25 @@
+from typing import Optional, Union, Callable
+from torch_scatter import scatter
+from einops import rearrange
 import numpy as np
 import torch
 import torch.nn
 import torch.fft
-import torch.nn.functional
+from torch.nn.functional import pad, fold
 from . import spectrogram
 
 import functools
 #import debugpy
 #debugpy.listen(("localhost", 5678))
+
+
 class MDCT(torch.nn.Module):
     """
     Serial version of the MDCT.
     Adapted from https://github.com/nils-werner/mdct/
     I modified the original code from scipy+numpy to torch, and I made it into torch layers with GPU support.
     """
+
     def __init__(self, window_function, step_length=None, n_fft=2048, center=True, device='cpu') -> None:
         super().__init__()
         self.window_function = window_function
@@ -26,7 +32,7 @@ class MDCT(torch.nn.Module):
         self,
         x,
         odd=True,
-        center = True,
+        center=True,
         **kwargs
     ):
         """ Calculate lapped MDCT of input signal
@@ -96,7 +102,8 @@ class MDCT(torch.nn.Module):
             n0 = (N + 1) / 2
             if odd:
                 outlen = N
-                pre_twiddle = torch.exp(-1j * np.pi * torch.arange(N * 2) / (N * 2)).to(self.device)
+                pre_twiddle = torch.exp(-1j * np.pi *
+                                        torch.arange(N * 2) / (N * 2)).to(self.device)
                 offset = 0.5
             else:
                 outlen = N + 1
@@ -155,7 +162,7 @@ class MDCT(torch.nn.Module):
                     functools.partial(_mdst, odd=False),
                 ],
                 halved=False,
-                frame_length = frame_length,
+                frame_length=frame_length,
                 **kwargs
             )
         else:
@@ -163,16 +170,18 @@ class MDCT(torch.nn.Module):
                 x,
                 transform=_mdct,
                 halved=False,
-                frame_length = frame_length,
+                frame_length=frame_length,
                 **kwargs
             )
 
     def forward(self, x):
-        x = self.mdct(x=x, window_function=self.window_function, step_length=self.step_length, n_fft=self.n_fft, center=self.center,padding = 0)
+        x = self.mdct(x=x, window_function=self.window_function,
+                      step_length=self.step_length, n_fft=self.n_fft, center=self.center, padding=0)
         return x
 
+
 class IMDCT(torch.nn.Module):
-    def __init__(self, window_function, step_length=None, device='cuda',n_fft=2048, out_length = 48000, center=True):
+    def __init__(self, window_function, step_length=None, device='cuda', n_fft=2048, out_length=48000, center=True):
         super().__init__()
         self.window_function = window_function
         self.step_length = step_length
@@ -283,7 +292,8 @@ class IMDCT(torch.nn.Module):
                 #Y[N+1:] = -1 * torch.conj(X[-2:0:-1])
                 Y[N+1:] = -1 * torch.conj(X[:-2].flip(dims=(0,)))
 
-            pre_twiddle = (torch.exp(1j * np.pi * n0 * torch.arange(N * 2) / N)).to(self.device)
+            pre_twiddle = (torch.exp(1j * np.pi * n0 *
+                           torch.arange(N * 2) / N)).to(self.device)
 
             y = torch.fft.ifft(Y.to(self.device) * pre_twiddle)
 
@@ -336,11 +346,13 @@ class IMDCT(torch.nn.Module):
                 X,
                 transform=_imdct,
                 halved=False,
-                frame_length = frame_length,
+                frame_length=frame_length,
                 **kwargs
             )
+
     def forward(self, X):
-        X = self.imdct(X=X, window_function=self.window_function, step_length=self.step_length, n_fft=self.n_fft, padding = 0, out_length = self.out_length)
+        X = self.imdct(X=X, window_function=self.window_function, step_length=self.step_length,
+                       n_fft=self.n_fft, padding=0, out_length=self.out_length)
         return X
 
 
@@ -349,6 +361,7 @@ class MDCT4(torch.nn.Module):
     The exact version of the MDCT, using modified DCT-IV.
     Borrowed from MATLAB implementation.
     """
+
     def __init__(self, n_fft=2048, hop_length=None, win_length=None, window=None, center=True, pad_mode='constant', device='cuda') -> None:
         super().__init__()
         self.n_fft = n_fft
@@ -367,27 +380,31 @@ class MDCT4(torch.nn.Module):
             self.window = window.to(self.device)
             self.win_length = len(window)
 
-        assert self.win_length <= self.n_fft, 'Window lenth %d should be no more than fft length %d'%(self.win_length, self.n_fft)
+        assert self.win_length <= self.n_fft, 'Window lenth %d should be no more than fft length %d' % (
+            self.win_length, self.n_fft)
         assert self.hop_length <= self.win_length, 'You hopped more than one frame'
 
-        self.exp1 = torch.exp(-1j*torch.pi/self.n_fft*torch.arange(start=0, end=self.n_fft,step=1, dtype=torch.float64, device=self.device))
-        self.exp2 = torch.exp(-1j*(torch.pi/(2*self.n_fft)+torch.pi/4)*torch.arange(start=1, end=self.n_fft, step=2, dtype=torch.float64, device=self.device))
+        self.exp1 = torch.exp(-1j*torch.pi/self.n_fft*torch.arange(start=0,
+                              end=self.n_fft, step=1, dtype=torch.float64, device=self.device))
+        self.exp2 = torch.exp(-1j*(torch.pi/(2*self.n_fft)+torch.pi/4)*torch.arange(
+            start=1, end=self.n_fft, step=2, dtype=torch.float64, device=self.device))
 
-    def forward(self, signal, return_frames:bool=False):
+    def forward(self, signal, return_frames: bool = False):
         # Pad the signal to a proper length
         signal_len = int(len(signal))
         start_pad = 0
         # Pad the signal so that the t-th frame is centered at time t * hop_length. Otherwise, the t-th frame begins at time t * hop_length.
         if self.center:
             start_pad = self.hop_length
-        additional_len = signal_len%self.hop_length
+        additional_len = signal_len % self.hop_length
         end_pad = start_pad
         if additional_len:
             end_pad = start_pad + self.hop_length - additional_len
-        signal = pad(signal, (start_pad,end_pad), mode=self.pad_mode)
+        signal = pad(signal, (start_pad, end_pad), mode=self.pad_mode)
 
         # Slice the signal with overlapping
-        signal = signal.unfold(dimension=-1, size=self.win_length, step=self.hop_length)
+        signal = signal.unfold(
+            dimension=-1, size=self.win_length, step=self.hop_length)
 
         # Apply windows to each pieces
         signal = torch.mul(signal, self.window)
@@ -398,17 +415,18 @@ class MDCT4(torch.nn.Module):
 
         # Pad zeros for DCT
         if self.n_fft > self.win_length:
-            signal = pad(signal, (0, self.n_fft-self.win_length), mode='constant')
+            signal = pad(signal, (0, self.n_fft-self.win_length),
+                         mode='constant')
 
         signal = signal*self.exp1
-        signal = torch.fft.fft(signal)[...,:self.n_fft//2]
+        signal = torch.fft.fft(signal)[..., :self.n_fft//2]
         signal = torch.real(self.exp2*signal)
 
         return signal, frames
 
 
 class IMDCT4(torch.nn.Module):
-    def __init__(self, n_fft=2048, hop_length=None, win_length=None, window=None, center=True, pad_mode='constant', out_length=None,device='cuda') -> None:
+    def __init__(self, n_fft=2048, hop_length=None, win_length=None, window=None, center=True, pad_mode='constant', out_length=None, device='cuda') -> None:
         super().__init__()
         self.n_fft = n_fft
         self.pad_mode = pad_mode
@@ -427,24 +445,29 @@ class IMDCT4(torch.nn.Module):
             self.window = window.to(self.device)
             self.win_length = len(window)
 
-        assert self.win_length <= self.n_fft, 'Window lenth %d should be no more than fft length %d'%(self.win_length, self.n_fft)
+        assert self.win_length <= self.n_fft, 'Window lenth %d should be no more than fft length %d' % (
+            self.win_length, self.n_fft)
         assert self.hop_length <= self.win_length, 'You hopped more than one frame'
 
-        self.exp1 = torch.exp(-1j*(torch.pi/(2*self.n_fft)+torch.pi/4)*torch.arange(start=1, end=self.n_fft, step=2, dtype=torch.float64, device=self.device))
-        self.exp2 = torch.exp(-1j*torch.pi/(2*self.n_fft)*torch.arange(start=0, end=2*self.n_fft,step=2, dtype=torch.float64, device=self.device))
+        self.exp1 = torch.exp(-1j*(torch.pi/(2*self.n_fft)+torch.pi/4)*torch.arange(
+            start=1, end=self.n_fft, step=2, dtype=torch.float64, device=self.device))
+        self.exp2 = torch.exp(-1j*torch.pi/(2*self.n_fft)*torch.arange(
+            start=0, end=2*self.n_fft, step=2, dtype=torch.float64, device=self.device))
 
-    def forward(self, signal, return_frames:bool=False):
-        assert signal.dim() == 3, 'Only tensors shaped in BHW are supported, got tensor of shape %s'%(str(signal.size()))
-        assert signal.size()[-1] == self.n_fft//2, 'The last dim of input tensor should match the n_fft. Expected %d ,got %d'%(self.n_fft, signal.size()[-1])
+    def forward(self, signal, return_frames: bool = False):
+        assert signal.dim() == 3, 'Only tensors shaped in BHW are supported, got tensor of shape %s' % (
+            str(signal.size()))
+        assert signal.size(
+        )[-1] == self.n_fft//2, 'The last dim of input tensor should match the n_fft. Expected %d ,got %d' % (self.n_fft, signal.size()[-1])
 
         # Inverse transform at the last dim
         signal = self.exp1*signal
-        signal = torch.fft.fft(signal,n=self.n_fft)
+        signal = torch.fft.fft(signal, n=self.n_fft)
         signal = torch.real(signal*self.exp2)
 
         # Remove padded zeros when doing dct
         if self.n_fft > self.win_length:
-            signal = signal[...,:self.win_length]
+            signal = signal[..., :self.win_length]
 
         # Apply windows to each pieces
         signal = torch.mul(signal, self.window)
@@ -455,18 +478,23 @@ class IMDCT4(torch.nn.Module):
 
         # Overlapping adding by fold()
         out_len = (signal.size()[-2]-1) * self.hop_length + self.win_length
-        signal = 4/self.n_fft*fold(signal.transpose_(-1,-2), kernel_size=(1,self.win_length), stride=(1,self.hop_length), output_size=(1,out_len))
+        signal = 4/self.n_fft*fold(signal.transpose_(-1, -2), kernel_size=(
+            1, self.win_length), stride=(1, self.hop_length), output_size=(1, out_len))
 
         if self.center:
             # extract the middle part
             signal = signal[..., self.win_length//2:-self.win_length//2]
-        signal = signal if self.out_length is None else signal[...,:self.out_length]
+        signal = signal if self.out_length is None else signal[...,
+                                                               :self.out_length]
         return signal, frames
 
-from typing import Optional, Union, Callable
-from einops import rearrange
-from torch_scatter import scatter
+
 class FastMDCT4(torch.nn.Module):
+    '''
+    Fast implementation of MDCT. Port to PyTorch by Chenhao Shuai
+    Ref:_https://ccrma.stanford.edu/~bosse/proj/node28.html
+    Sporer T, Brandenburg K, Edler B, The Use of Multirate Filter Banks for Coding of High Quality Digital Audio, 6th European Signal Processing Conference (EUSIPCO), Amsterdam, June 1992, Vol.1 pp. 211-214.
+    '''
     def __init__(self, n_fft: Optional[int] = 2048, hop_length: Optional[int] = None, win_length: Optional[int] = None, window: Union[torch.Tensor, np.ndarray, list, Callable, None] = None, center: bool = True, pad_mode: str = 'constant', device: str = 'cuda') -> None:
         super().__init__()
         self.n_fft = n_fft
@@ -548,12 +576,15 @@ class FastMDCT4(torch.nn.Module):
         return sgn.roll(-self.n_fft//4, dims=-1).contiguous()
 
     def make_pre_idx(self):
-        i = torch.arange(start=0, end=self.n_fft, step=1, dtype=torch.long, device=self.device)
-        i = i.roll(self.n_fft//4,dims=-1)
-        idx_ = torch.stack([i[self.idx[0]], i[self.idx[1]], i[self.idx[2]], i[self.idx[3]]],dim=1)
-        index = torch.zeros(1,self.n_fft,device=self.device, dtype=torch.long)
-        for i in torch.arange(0,self.n_fft//4,dtype=torch.long):
-          index[...,idx_[i]]=i
+        i = torch.arange(start=0, end=self.n_fft, step=1,
+                         dtype=torch.long, device=self.device)
+        i = i.roll(self.n_fft//4, dims=-1)
+        idx_ = torch.stack([i[self.idx[0]], i[self.idx[1]],
+                           i[self.idx[2]], i[self.idx[3]]], dim=1)
+        index = torch.zeros(
+            1, self.n_fft, device=self.device, dtype=torch.long)
+        for i in torch.arange(0, self.n_fft//4, dtype=torch.long):
+            index[..., idx_[i]] = i
         return index.squeeze().contiguous()
 
     def make_post_idx(self):
@@ -563,9 +594,9 @@ class FastMDCT4(torch.nn.Module):
         return idx.flatten().contiguous()
 
     def forward(self, signal: torch.tensor, return_frames: bool = False):
-        if signal.dim() == 2: # B T (mono)
+        if signal.dim() == 2:  # B T (mono)
             signal = signal[:, None, :]
-        elif signal.dim() == 3: # B C T (stereo)
+        elif signal.dim() == 3:  # B C T (stereo)
             pass
         else:
             raise NotImplementedError
@@ -583,10 +614,11 @@ class FastMDCT4(torch.nn.Module):
         signal = pad(signal, (start_pad, end_pad), mode=self.pad_mode)
 
         # Slice the signal with overlapping
-        signal = signal.unfold(dimension=-1, size=self.win_length, step=self.hop_length)
+        signal = signal.unfold(
+            dimension=-1, size=self.win_length, step=self.hop_length)
         signal = signal*self.pre_exp
         signal = scatter(signal, self.pre_idx, dim=-1, reduce='sum')
-        signal = torch.fft.fft(signal,dim=-1)
+        signal = torch.fft.fft(signal, dim=-1)
         # post-twiddle
         signal = torch.conj_physical(signal*self.post_exp)
         # rearranging
@@ -645,7 +677,8 @@ class FastIMDCT4(torch.nn.Module):
         ).contiguous()
         self.pre_idx = self.make_pre_idx()
         self.post_idx = self.make_post_index()
-        self.window = (4.0*self.make_sign()*self.window/self.n_fft).to(torch.float32).contiguous()
+        self.window = (4.0*self.make_sign()*self.window /
+                       self.n_fft).to(torch.float32).contiguous()
 
     def make_pre_idx(self):
         a = torch.arange(self.n_fft//2, dtype=torch.long,
